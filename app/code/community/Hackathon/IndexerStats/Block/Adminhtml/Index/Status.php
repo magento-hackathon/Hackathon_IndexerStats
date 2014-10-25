c74b29263256e5979b35c76f3fcc0ee564252ccc
Fabian Schmengler <fs@integer-net.de>
2014-10-25 23:12:51 +0200

    Show progress bar for running indexer processes

--- app/code/community/Hackathon/IndexerStats/Block/Adminhtml/Index/Status.php
+++ app/code/community/Hackathon/IndexerStats/Block/Adminhtml/Index/Status.php
@@ -18,14 +18,50 @@ class Hackathon_IndexerStats_Block_Adminhtml_Index_Status extends Mage_Adminhtml
 
 // Magento Hackathon Tag NEW_CONST
 
+    /**
+     * @var Hackathon_IndexerStats_Model_Runtime
+     */
+    protected $_runtimeModel;
+
 // Magento Hackathon Tag NEW_VAR
+
+    protected function _construct()
+    {
+        $this->_runtimeModel = Mage::getModel('hackathon_indexerstats/runtime');
+        parent::_construct();
+    }
+
 
     public function render(Varien_Object $row)
     {
-        return Mage::getModel('hackathon_indexerstats/runtime')
-            ->getAvgRuntime($row);
+        /** @var Mage_Index_Model_Process $row */
+        if ($row->getStatus() === Mage_Index_Model_Process::STATUS_RUNNING) {
+            return $this->_renderProgress($row);
+        } else {
+            return $this->_renderAvgRuntime($row);
+        }
     }
 
+    protected function _renderAvgRuntime(Mage_Index_Model_Process $process)
+    {
+        return $this->_runtimeModel->getAvgRuntime($process);
+    }
+
+    protected function _renderProgress(Mage_Index_Model_Process $process)
+    {
+        $progress = min(100, $this->_runtimeModel->getProgress($process) * 100);
+        if ($this->_runtimeModel->getProgress($process) > 1) {
+            $inTimeClass = 'hackathon_indexerstats_not_in_time';
+        } else {
+            $inTimeClass = 'hackathon_indexerstats_in_time';
+        }
+        $remainingTime = $this->_runtimeModel->getRemainingTime($process);
+
+        return <<<HTML
+<div class="hackathon_indexerstats_progress {$inTimeClass}"><span style="width: {$progress}%;"><span></span></span><div>{$remainingTime}</div></div>
+HTML;
+
+    }
 
 // Magento Hackathon Tag NEW_METHOD
 
