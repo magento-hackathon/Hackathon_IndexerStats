925f2931c8a172484478092c598a5919f47c8372
Fabian Schmengler <fs@integer-net.de>
2014-10-26 14:52:42 +0100

    Only use the last 5 history entries for estimation

--- app/code/community/Hackathon/IndexerStats/Model/Resource/History.php
+++ app/code/community/Hackathon/IndexerStats/Model/Resource/History.php
@@ -16,6 +16,7 @@
 class Hackathon_IndexerStats_Model_Resource_History extends Mage_Core_Model_Resource_Db_Abstract
 {
 
+    const XML_PATH_NUMBER_FOR_AVG = 'hackathon_indexerstats/process/number_for_avg';
 // Magento Hackathon Tag NEW_CONST
 
 // Magento Hackathon Tag NEW_VAR
@@ -32,22 +33,42 @@ class Hackathon_IndexerStats_Model_Resource_History extends Mage_Core_Model_Reso
     public function getAvg($processId)
     {
         $adapter = $this->_getReadAdapter();
-        $select = $adapter->select();
-        $select->from(array("history" => $this->getTable('hackathon_indexerstats/history')), array('avg_running_time' => 'AVG(running_time)'))
-            ->where('process_id = :process_id');
 
+        $selectLastEntries = $adapter->select()
+            ->from(
+                array("history" => $this->getTable('hackathon_indexerstats/history')),
+                array('running_time' => 'running_time'))
+            ->where('process_id = :process_id')
+            ->order('history_id DESC')
+            ->limit($this->_getNumberOfRowsUsedForAverage());
+
+        $selectAverage = $adapter->select();
+        $selectAverage->from(
+            array("dataset" => new Zend_Db_Expr("($selectLastEntries)")),
+            array('avg_running_time' => 'AVG(dataset.running_time)'));
+
+        Mage::log($selectAverage->__toString());
         $bind = array(
             'process_id' => $processId,
         );
 
-        $avgTime = $adapter->fetchOne($select, $bind);
+        $avgTime = $adapter->fetchOne($selectAverage, $bind);
 
         if (!empty($avgTime)) {
             return (int)$avgTime;
         } else {
             return 0;
         }
+    }
 
+    /**
+     * Returns how many history rows should be used to calculate the estimated time
+     *
+     * @return int
+     */
+    protected function _getNumberOfRowsUsedForAverage()
+    {
+        return (int) Mage::getStoreConfig(self::XML_PATH_NUMBER_FOR_AVG);
     }
 
 // Magento Hackathon Tag NEW_METHOD
